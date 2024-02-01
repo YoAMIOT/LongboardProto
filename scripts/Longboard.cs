43d571acc91c253547b7f6d4e861ac5709bdea80
YoAMIOT <yoann.amiot.dev@gmail.com>
2024-02-01 14:03:45 +0100

    Added a new testing track and change the thrust forward to something more realistic, added braking

--- scripts/Longboard.cs
+++ scripts/Longboard.cs
@@ -2,8 +2,9 @@ using Godot;
 using System;
 
 public partial class Longboard : VehicleBody3D{
-	private const float MAX_STEER = 0.25f;
-	private const float ENGINE_POWER = 3;
+	private const float MAX_STEER = 0.2f;
+	private const float ENGINE_POWER = 10;
+	private const float BRAKE_POWER = 0.08f;
 	private Node3D CameraPivot;
 	private Camera3D Camera;
 	private VehicleWheel3D FrontRightWheel;
@@ -11,6 +12,7 @@ public partial class Longboard : VehicleBody3D{
 	private VehicleWheel3D BackRightWheel;
 	private VehicleWheel3D BackLeftWheel;
 	private MeshInstance3D Board;
+	private Timer ThrustCooldown;
 
 	public override void _Ready(){
 		Input.MouseMode = Input.MouseModeEnum.Captured;
@@ -21,18 +23,32 @@ public partial class Longboard : VehicleBody3D{
 		BackRightWheel = GetNode<VehicleWheel3D>("BackRight");
 		BackLeftWheel = GetNode<VehicleWheel3D>("BackLeft");
 		Board = GetNode<MeshInstance3D>("Board");
+		ThrustCooldown = GetNode<Timer>("ThrustCooldown");
 	}
 
 	public override void _PhysicsProcess(double delta){
-		float coef = (float)delta * 1.2f;
-		FrontRightWheel.Steering = (float)Mathf.MoveToward(FrontRightWheel.Steering, Input.GetAxis("Right","Left") * MAX_STEER, coef);
-		FrontLeftWheel.Steering = (float)Mathf.MoveToward(FrontLeftWheel.Steering, Input.GetAxis("Right","Left") * MAX_STEER, coef);
-		BackRightWheel.Steering = (float)Mathf.MoveToward(BackRightWheel.Steering, Input.GetAxis("Left","Right") * MAX_STEER, coef);
-		BackLeftWheel.Steering = (float)Mathf.MoveToward(BackLeftWheel.Steering, Input.GetAxis("Left","Right") * MAX_STEER, coef);
-		float BoardRotation =  (float)Mathf.MoveToward(Board.Rotation.X, Input.GetAxis("Left","Right") * MAX_STEER, coef);
+		float STEERING_SPEED = (float)delta * 0.8f;
+		FrontRightWheel.Steering = (float)Mathf.MoveToward(FrontRightWheel.Steering, Input.GetAxis("Right","Left") * MAX_STEER, STEERING_SPEED);
+		FrontLeftWheel.Steering = (float)Mathf.MoveToward(FrontLeftWheel.Steering, Input.GetAxis("Right","Left") * MAX_STEER, STEERING_SPEED);
+		BackRightWheel.Steering = (float)Mathf.MoveToward(BackRightWheel.Steering, Input.GetAxis("Left","Right") * MAX_STEER, STEERING_SPEED);
+		BackLeftWheel.Steering = (float)Mathf.MoveToward(BackLeftWheel.Steering, Input.GetAxis("Left","Right") * MAX_STEER, STEERING_SPEED);
+		float BoardRotation =  (float)Mathf.MoveToward(Board.Rotation.X, Input.GetAxis("Left","Right") * MAX_STEER, STEERING_SPEED);
 		Board.Rotation = new Vector3(BoardRotation, 0, 0);
-		this.EngineForce = Input.GetAxis("Backward", "Forward") * ENGINE_POWER;
 		CameraPivot.GlobalPosition = CameraPivot.GlobalPosition.Lerp(this.GlobalPosition, (float)delta * 20);
 
+		//Thrust management
+		if (ThrustCooldown.TimeLeft > 1f && Input.IsActionPressed("Forward")){
+			this.EngineForce = ENGINE_POWER;
+			GetNode<MeshInstance3D>("Cap").Visible = true;
+		} else {
+			this.EngineForce = 0;
+			GetNode<MeshInstance3D>("Cap").Visible = false;
+		}
+		
+		if (Input.IsActionPressed("Backward") && !Input.IsActionPressed("Forward")){
+			this.Brake = BRAKE_POWER;
+		} else {
+			this.Brake = 0;
+		}
 	}
 }
