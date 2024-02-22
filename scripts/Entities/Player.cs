9b4a13f343e95cd9b1335507bff51e5c7dd117bd
YoAMIOT <yoann.amiot.dev@gmail.com>
2024-02-22 15:06:29 +0100

    Player can now get on the Longboard

--- scripts/Entities/Player.cs
+++ scripts/Entities/Player.cs
@@ -8,42 +8,55 @@ public partial class Player : CharacterBody3D {
 	private Camera Camera;
 	private Node3D Armature;
 	private AnimationTree AnimTree;
+	private Node3D LastInteractedObject;
+	private CollisionShape3D Collision;
+	private bool onBoard = false; 
 
 
 	public override void _Ready(){
 		Camera = GetNode<Camera>("CameraPivotH");
 		Armature = GetNode<Node3D>("Armature");
 		AnimTree = GetNode<AnimationTree>("AnimationTree");
+		Collision = GetNode<CollisionShape3D>("Collision");
 		Camera.InteractedWith += InteractWith;
 	}
 
 	public override void _PhysicsProcess(double delta){
-		Vector3 velocity = Velocity;
+		if (!onBoard){
+			Vector3 velocity = Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor()){
-			velocity.Y -= gravity * (float)delta;
-		}
+			// Add the gravity.
+			if (!IsOnFloor()){
+				velocity.Y -= gravity * (float)delta;
+			}
 
-		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		direction = direction.Rotated(Vector3.Up, Mathf.DegToRad(Camera.RotationDegrees.Y));
-		if (direction != Vector3.Zero){
-			velocity.X = Mathf.Lerp(velocity.X, direction.X * SPEED, LERP_VALUE);
-			velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * SPEED, LERP_VALUE);
-			Armature.Rotation = new Vector3(0, Mathf.LerpAngle(Armature.Rotation.Y, Mathf.Atan2(-velocity.X, -velocity.Z), LERP_VALUE), 0);
+			Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
+			Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+			direction = direction.Rotated(Vector3.Up, Mathf.DegToRad(Camera.RotationDegrees.Y));
+			if (direction != Vector3.Zero){
+				velocity.X = Mathf.Lerp(velocity.X, direction.X * SPEED, LERP_VALUE);
+				velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * SPEED, LERP_VALUE);
+				Armature.Rotation = new Vector3(0, Mathf.LerpAngle(Armature.Rotation.Y, Mathf.Atan2(-velocity.X, -velocity.Z), LERP_VALUE), 0);
+			} else {
+				velocity.X = Mathf.Lerp(velocity.X, 0f, LERP_VALUE);
+				velocity.Z = Mathf.Lerp(velocity.Z, 0f, LERP_VALUE);
+			}
+
+			AnimTree.Set("parameters/BlendSpace1D/blend_position", velocity.Length() /SPEED);
+
+			Velocity = velocity;
+			MoveAndSlide();
 		} else {
-			velocity.X = Mathf.Lerp(velocity.X, 0f, LERP_VALUE);
-			velocity.Z = Mathf.Lerp(velocity.Z, 0f, LERP_VALUE);
+			Node3D LongboardMarker = (Node3D)LastInteractedObject.GetChild(0);
+			this.GlobalTransform = LongboardMarker.GlobalTransform;
 		}
-
-		AnimTree.Set("parameters/BlendSpace1D/blend_position", velocity.Length() /SPEED);
-
-		Velocity = velocity;
-		MoveAndSlide();
 	}
 
-	private void InteractWith(Node Target){
-		//DO MAGIC HERE
+	private void InteractWith(Node3D Target){
+		LastInteractedObject = Target;
+		if (LastInteractedObject.GetType() == typeof(Longboard)){
+			onBoard = true;
+			Collision.Disabled = true;
+		}
 	}
 }
--- scripts/Systems/Camera.cs
+++ scripts/Systems/Camera.cs
@@ -5,23 +5,19 @@ public partial class Camera : Node3D{
 	private const int MIN_PITCH = -50;
 	private const int MAX_PITCH = 30;
 	private float MouseSensivity = 0.05f;
-	private bool recenterCamera = false;
 	private string parentName = "";
 	private Node3D CameraPivotV;
 	private Camera3D Cam;
-	private Timer RecenterCameraTimer;
 	private RayCast3D InteractionRayCast;
 	private Label InteractionLabel;
 
 	[Signal]
-	public delegate void InteractedWithEventHandler(Node Target);
+	public delegate void InteractedWithEventHandler(Node3D Target);
 
 	public override void _Ready(){
 		Input.MouseMode = Input.MouseModeEnum.Captured;
 		CameraPivotV = GetNode<Node3D>("CameraPivotV");
 		Cam = GetNode<Camera3D>("CameraPivotV/CameraSpringArm/Camera3D");
-		RecenterCameraTimer = GetNode<Timer>("ReCenterCamera");
-		RecenterCameraTimer.Timeout += TiggerRecenterCamera;
 		InteractionRayCast = GetNode<RayCast3D>("CameraPivotV/CameraSpringArm/RayCast3D");
 		InteractionLabel = GetNode<Label>("HUD/InteractionLabel");
 		parentName = GetParent().Name;
@@ -32,42 +28,18 @@ public partial class Camera : Node3D{
 			this.RotateY(-Mathf.DegToRad(mouseMotion.Relative.X) * MouseSensivity);
 			CameraPivotV.RotateX(-Mathf.DegToRad(mouseMotion.Relative.Y) * MouseSensivity);
 			CameraPivotV.RotationDegrees = new Vector3(Mathf.Clamp(CameraPivotV.RotationDegrees.X, MIN_PITCH, MAX_PITCH), 0, 0);
-			RecenterCameraTimer.WaitTime = 0.7f;
-			RecenterCameraTimer.Start();
 		}
     }
 
 	public override void _Process(double delta){
-		if(recenterCamera && parentName == "Longboard"){
-			RecenterCamera(delta);
-		}
 		if (InteractionRayCast.IsColliding()){
-			Node TargetObject = ((Node3D)InteractionRayCast.GetCollider()).GetParent();
+			Node3D TargetObject = (Node3D)((Node3D)InteractionRayCast.GetCollider()).GetParent();
 			InteractionLabel.Text = TargetObject.Name;
 			if (Input.IsActionJustPressed("Interact")){
 				EmitSignal(nameof(this.InteractedWith), TargetObject);
 			}
 		} else {
 			InteractionLabel.Text = "";
-		}
-	}
-
-	private void TiggerRecenterCamera(){
-		recenterCamera = true;
-	}
-
-	//Manage recentering
-	private void RecenterCamera(double delta){
-		if(this.RotationDegrees.Y != 0){
-			float HorizontalRotation = (float)Mathf.MoveToward(this.RotationDegrees.Y, 0, delta * 120);
-			this.RotationDegrees = new Vector3(0, HorizontalRotation, 0);
-		}
-		if (CameraPivotV.RotationDegrees.Z != 0){
-			float VerticalRotation = (float)Mathf.MoveToward(CameraPivotV.RotationDegrees.Z, 0, delta * 120);
-			CameraPivotV.RotationDegrees = new Vector3(0, 0, VerticalRotation);
-		}
-		if (this.RotationDegrees.Y == 0 && CameraPivotV.RotationDegrees.Z == 0){
-			recenterCamera = false;
 		}
 	}
 }
