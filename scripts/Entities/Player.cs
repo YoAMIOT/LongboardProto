ba21d94436f14c1b6e75b57af3ed9a35fc522095
YoAMIOT <yoann.amiot.dev@gmail.com>
2024-02-23 15:39:17 +0100

    Started to work with a rigidbody player instead

--- scripts/Entities/Player.cs
+++ scripts/Entities/Player.cs
@@ -1,43 +1,42 @@
 using Godot;
 using System;
 
-public partial class Player : CharacterBody3D {
-	private const float SPEED = 3f;
+public partial class Player : RigidBody3D {
+	private const float SPEED = 4f;
 	private const float LERP_VALUE = 0.15f;
-	private Camera Camera;
 	private Node3D Armature;
+	private Camera Camera;
 	private AnimationTree AnimTree;
-	private Node3D LastInteractedObject;
 	private CollisionShape3D Collision;
-	private float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+	private Node3D LastInteractedObject;
 	public bool onBoard = false;
 	private bool canInteract = true;
 
+
 	public override void _Ready(){
-		Camera = GetNode<Camera>("CameraPivotH");
 		Armature = GetNode<Node3D>("Armature");
+		Camera = GetNode<Camera>("CameraPivotH");
 		AnimTree = GetNode<AnimationTree>("AnimationTree");
 		Collision = GetNode<CollisionShape3D>("Collision");
 	}
 
-	public override void _PhysicsProcess(double delta){
-		Vector3 velocity = Velocity;
+    public override void _IntegrateForces(PhysicsDirectBodyState3D state){
+        Vector3 velocity = LinearVelocity;
 		if (!onBoard){
-			if (!IsOnFloor()){
-				velocity.Y -= gravity * (float)delta;
-			}
 
-			Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
-			Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-			direction = direction.Rotated(Vector3.Up, Mathf.DegToRad(Camera.RotationDegrees.Y));
-			if (direction != Vector3.Zero){
-				velocity.X = Mathf.Lerp(velocity.X, direction.X * SPEED, LERP_VALUE);
-				velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * SPEED, LERP_VALUE);
-				Armature.Rotation = new Vector3(0, Mathf.LerpAngle(Armature.Rotation.Y, Mathf.Atan2(-velocity.X, -velocity.Z), LERP_VALUE), 0);
-			} else {
-				velocity.X = Mathf.Lerp(velocity.X, 0f, LERP_VALUE);
-				velocity.Z = Mathf.Lerp(velocity.Z, 0f, LERP_VALUE);
-			}
+		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		direction = direction.Rotated(Vector3.Up, Mathf.DegToRad(Camera.RotationDegrees.Y));
+		if (direction != Vector3.Zero){
+			velocity.X = Mathf.Lerp(velocity.X, direction.X * SPEED, LERP_VALUE);
+			velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * SPEED, LERP_VALUE);
+			Armature.Rotation = new Vector3(0, Mathf.LerpAngle(Armature.Rotation.Y, Mathf.Atan2(-velocity.X, -velocity.Z), LERP_VALUE), 0);
+		} else {
+			velocity.X = Mathf.Lerp(velocity.X, 0f, LERP_VALUE);
+			velocity.Z = Mathf.Lerp(velocity.Z, 0f, LERP_VALUE);
+		}
+		AnimTree.Set("parameters/BlendSpace1D/blend_position", velocity.Length() /SPEED);
+		LinearVelocity = velocity;
 		} else {
 			velocity = new Vector3();
 			Node3D LongboardMarker = (Node3D)LastInteractedObject.GetChild(0);
@@ -47,26 +46,23 @@ public partial class Player : CharacterBody3D {
 				InteractWith(LastInteractedObject);
 			}
 		}
-		AnimTree.Set("parameters/BlendSpace1D/blend_position", velocity.Length() /SPEED);
-		Velocity = velocity;
-		MoveAndSlide();
-		GD.Print(onBoard);
-	}
+    }
 
-	public async void InteractWith(Node3D Target){
+		public async void InteractWith(Node3D Target){
 		LastInteractedObject = Target;
-		if (LastInteractedObject.GetType() == typeof(Longboard)){
+		if (Target.GetType() == typeof(Longboard)){
 			if(!onBoard){
 				canInteract = false;
 				Collision.Disabled = true;
 				Target.Call("SetInUse", true);
 				onBoard = true;
-				await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
+				await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
 				canInteract = true;
 			} else {
 				Collision.Disabled = false;
 				Target.Call("SetInUse", false);
-				//await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
+				Node3D LongboardExitMarker = (Node3D)Target.GetChild(1);
+				this.GlobalPosition = LongboardExitMarker.GlobalPosition;
 				this.GlobalRotation = new Vector3();
 				onBoard = false;
 			}
