1b5c4fe0a4333ba05294b8f277c0e1c4aea601d8
YoAMIOT <yoann.amiot.dev@gmail.com>
2024-02-24 08:54:09 +0100

    fix on the delay between the player armature and the acceleration of the board

--- scripts/Entities/Player.cs
+++ scripts/Entities/Player.cs
@@ -20,32 +20,37 @@ public partial class Player : RigidBody3D {
 		Collision = GetNode<CollisionShape3D>("Collision");
 	}
 
-    public override void _IntegrateForces(PhysicsDirectBodyState3D state){
-        Vector3 velocity = LinearVelocity;
-		if (!onBoard){
-
-		Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		direction = direction.Rotated(Vector3.Up, Mathf.DegToRad(Camera.RotationDegrees.Y));
-		if (direction != Vector3.Zero){
-			velocity.X = Mathf.Lerp(velocity.X, direction.X * SPEED, LERP_VALUE);
-			velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * SPEED, LERP_VALUE);
-			Armature.Rotation = new Vector3(0, Mathf.LerpAngle(Armature.Rotation.Y, Mathf.Atan2(-velocity.X, -velocity.Z), LERP_VALUE), 0);
-		} else {
-			velocity.X = Mathf.Lerp(velocity.X, 0f, LERP_VALUE);
-			velocity.Z = Mathf.Lerp(velocity.Z, 0f, LERP_VALUE);
-		}
-		AnimTree.Set("parameters/BlendSpace1D/blend_position", velocity.Length() /SPEED);
-		LinearVelocity = velocity;
-		} else {
-			velocity = new Vector3();
+    public override void _Process(double delta){
+        if (onBoard){
 			Node3D LongboardMarker = (Node3D)LastInteractedObject.GetChild(0);
 			Armature.Rotation = new Vector3(LongboardMarker.Rotation.X, Mathf.LerpAngle(Armature.Rotation.Y, LongboardMarker.Rotation.Y - Mathf.DegToRad(90f), LERP_VALUE), LongboardMarker.Rotation.Z);
 			this.GlobalTransform = LongboardMarker.GlobalTransform;
+
+		}
+    }
+
+    public override void _IntegrateForces(PhysicsDirectBodyState3D state){
+        Vector3 velocity = LinearVelocity;
+		if (!onBoard){
+			Vector2 inputDir = Input.GetVector("Left", "Right", "Forward", "Backward");
+			Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+			direction = direction.Rotated(Vector3.Up, Mathf.DegToRad(Camera.RotationDegrees.Y));
+			if (direction != Vector3.Zero){
+				velocity.X = Mathf.Lerp(velocity.X, direction.X * SPEED, LERP_VALUE);
+				velocity.Z = Mathf.Lerp(velocity.Z, direction.Z * SPEED, LERP_VALUE);
+				Armature.Rotation = new Vector3(0, Mathf.LerpAngle(Armature.Rotation.Y, Mathf.Atan2(-velocity.X, -velocity.Z), LERP_VALUE), 0);
+			} else {
+				velocity.X = Mathf.Lerp(velocity.X, 0f, LERP_VALUE);
+				velocity.Z = Mathf.Lerp(velocity.Z, 0f, LERP_VALUE);
+			}
+		} else {
+			velocity = new Vector3();
 			if (Input.IsActionJustPressed("Interact") && canInteract){
 				InteractWith(LastInteractedObject);
 			}
 		}
+		AnimTree.Set("parameters/BlendSpace1D/blend_position", velocity.Length() /SPEED);
+		LinearVelocity = velocity;
     }
 
 		public async void InteractWith(Node3D Target){
